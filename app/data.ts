0eb8350a97bf742db0a6682ed08c2bc92c53d22e
Pedro Pablo Moreno <ppmoreno@gmail.com>
2025-10-16 12:14:45 +0200

    chore: sincroniza cambios recientes en data

--- app/data.ts
+++ app/data.ts
@@ -31,17 +31,15 @@ export const PROJECTS: Project[] = [
   {
     name: 'WEA Scout Pro',
     description:
-      'Aplicación integral de gestión y análisis futbolístico, diseñada para clubes, entrenadores y ojeadores. Combina herramientas de scouting, control de rendimiento, estadísticas avanzadas e inteligencia artificial en una experiencia multidispositivo, intuitiva y visual.
-
-Su objetivo es ofrecer una visión completa del juego, desde la detección de talento hasta el seguimiento táctico y físico de los jugadores, centralizando toda la información deportiva en un solo entorno digital.',
+      'Aplicación integral de gestión y análisis futbolístico, diseñada para clubes, entrenadores y ojeadores. Combina herramientas de scouting, control de rendimiento, estadísticas avanzadas e inteligencia artificial en una experiencia multidispositivo, intuitiva y visual. Su objetivo es ofrecer una visión completa del juego, desde la detección de talento hasta el seguimiento táctico y físico de los jugadores, centralizando toda la información deportiva en un solo entorno digital.',
     link: 'https://pro.motion-primitives.com/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
     id: 'project1',
   },
   {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
+    name: 'Futplay 360',
+    description: 'Plataforma multidispositivo de formación online especializada en fútbol, que reúne cursos profesionales de scouting, dirección deportiva y análisis de juego impartidos por referentes del sector. Combina una experiencia de aprendizaje moderna con contenidos multimedia, evaluaciones interactivas e inteligencia artificial aplicada al estudio, ofreciendo una forma práctica y accesible de formarse en el ámbito futbolístico desde cualquier lugar.',
     link: 'https://motion-primitives.com/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
