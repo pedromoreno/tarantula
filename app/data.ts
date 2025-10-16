1df03500141630020392b4575542ce3aa55b81de
Pedro Pablo Moreno <ppmoreno@gmail.com>
2025-10-16 14:34:53 +0200

    Actualizar data.ts

--- app/data.ts
+++ app/data.ts
@@ -92,7 +92,7 @@ export const BLOG_POSTS: BlogPost[] = [
 export const SOCIAL_LINKS: SocialLink[] = [
   {
     label: 'Fotografías',
-    link: 'https://pedromoreno.mypixieset.com',
+    link: 'https://pedromoreno.mypixieset.com/portfolio/',
   },
   {
     label: 'Twitter',
