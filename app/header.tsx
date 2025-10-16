24c7be913887508505cefea1abe10002dde6551f
Pedro Pablo Moreno <ppmoreno@gmail.com>
2025-10-16 11:39:39 +0200

    chore: actualiza subtítulo a 'Estudio Creativo' en header

--- app/header.tsx
+++ app/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
           className="text-zinc-600 dark:text-zinc-500"
           delay={0.5}
         >
-          Design Engineer
+          Estudio Creativo
         </TextEffect>
       </div>
     </header>
